669efa9ef76b539d0f0f8e6084143e0b5d505a61
Josefina Forcada <forcadajosefina@gmail.com>
2021-03-07 12:01:16 -0300

    Primer commit- Inclui componente navbar, cart Widget e item list container

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ItemListContainer from './components/ItemListContainer';
+import NavBar from './components/NavBar';
+
+
+const App = () => (
+  <>
+    <NavBar/>
+    <ItemListContainer title="ACA VAN A IR LAS CARDS CON LOS PRODUCTOS" />
+  </>
+);
+
+export default App;
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+const ItemListContainer = (props) => (
+    <>
+        <main className="container-fluid vh-100 d-flex justify-content-center">
+            <h2 className="text-center align-self-center">{props.title}</h2>
+        </main>
+    </>
+);
+
+export default ItemListContainer;
